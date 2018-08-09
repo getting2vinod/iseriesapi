fa225f064b0f1b086070ca8a2cc46e2e9b659e31
Vinod <Vinod.nair@relevancelab.com>
2018-08-09 16:11:04 +0530

    Included resource file

--- src/main/java/as400pwdrest/asuser.java
+++ src/main/java/as400pwdrest/asuser.java
@@ -4,16 +4,20 @@ import com.ibm.as400.access.*;
 import javax.annotation.Resource;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.Properties;
-import java.util.ResourceBundle;
+
+
 
 public class asuser {
 
-    private static ResourceBundle rb = ResourceBundle.getBundle("app");
+    private static Properties prop = new Properties();
 
-    public static void main(String args[]) {
+
+
+    public static void main(String args[]) throws Exception {
 //        String systemName = args[0];
 //        String userId = args[1];
 //        String password = args[2];
@@ -22,17 +26,30 @@ public class asuser {
        // appProps.load(new FileInputStream(appConfigPath));
 
        // prop.setProperty("")
-        rb.
-        System.out.println(rb.getString("server") + rb.getString("username") +rb.getString("password"));
+        System.out.println(System.getProperty("user.dir"));
+        prop.load(new FileInputStream(new File(System.getProperty("user.dir")+"\\app.properties")));
 
-        AS400 as400 = new AS400(rb.getString("server"),rb.getString("username"),rb.getString("password"));
+
+
+
         if(args.length <= 0){
-            System.out.println("Insufficient arguments passed. \nPlease do provide the <userprofile> and <password> to update");
+            System.out.println("Insufficient arguments passed. \nPlease do provide the <userprofile>  <password> <server> to update");
             return;
         }
         String userId = args[0];
         String password = args[1];
+        String system = args[2];
 
+        //Checking if the property is available
+        if(prop.getProperty("server."+system) == null){
+            System.out.println("Server resource not found. "+system);
+            return;
+        }
+
+
+        System.out.println(prop.getProperty("server."+system) + prop.getProperty("username."+system) +prop.getProperty("password."+system).length());
+
+        AS400 as400 = new AS400(prop.getProperty("server."+system),prop.getProperty("username."+system),prop.getProperty("password."+system));
 
         try {
 
@@ -41,8 +58,8 @@ public class asuser {
             System.out.println("Connected:"+as400.isConnected());
 
             CommandCall cmd = new CommandCall(as400);
-            String cmdTxt = "dspusrprf usrprf("+userId+")";
-           // String cmdTxt = "chgusrprf usrprf("+userId+") password("+password+") status(*enabled)";
+            //String cmdTxt = "dspusrprf usrprf("+userId+")";
+            String cmdTxt = "chgusrprf usrprf("+userId+") password("+password+") status(*enabled)";
               //    cmdTxt ="chgusrprf usrprf(CCGV5R2EYO) password(OKTABOT) status(*enabled)";
             if(cmd.run(cmdTxt) != true){
                 System.out.println("Not a valid command");
